d253ce570724f7d4710bf550feee78a3addc4888
kaique <caires.kaique@gmail.com>
2024-10-06 07:51:08 -0300

    chore: added files to deployment

--- server.go
+++ server.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/config-map", ConfigMap)
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
@@ -19,4 +21,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	w.Write([]byte(fmt.Sprintf("Hello, my name is %s, and I am %s years old", name, age)))
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("error reading file: %s", err.Error())
+	}
+	fmt.Fprintf(w, "My Family: %s", string(data))
 }
