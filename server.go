99c0e08df5652806797b0570a385763d7372fed9
kaique <caires.kaique@gmail.com>
2024-10-07 05:31:35 -0300

    chore: added secret

--- server.go
+++ server.go
@@ -20,7 +20,8 @@ func main() {
 func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
-	w.Write([]byte(fmt.Sprintf("Hello, my name is %s, and I am %s years old", name, age)))
+	secret := os.Getenv("SECRET")
+	w.Write([]byte(fmt.Sprintf("Hello, my name is %s, and I am %s years old, this is my secret: %s", name, age, secret)))
 }
 
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
